bd6b9fe3969f28ca8d0de31f9dc8b4be71d42916
gleatd01 <Gleason.usa@gmail.com>
2021-06-11 01:02:41 -0400

    working lat long

--- env/serial_gps.py
+++ env/serial_gps.py
@@ -3,4 +3,11 @@ with serial.Serial('/dev/ttyACM0', baudrate=4800, timeout=1) as ser:
     #x = ser.read()
     #s = ser.read(10)
     line = ser.readline()
-print(line)
+    str_line=str(line)
+    split_line=str_line.split(',');
+    lat = split_line[3],split_line[4]
+    long =split_line[5],split_line[6]
+print(split_line[3],split_line[4])
+print(split_line[5],split_line[6])
+print(lat)
+print(long)
