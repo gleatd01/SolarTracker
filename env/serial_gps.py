1ad3909f16d0bf540cc41f2d045dd35826d750e1
gleatd01 <Gleason.usa@gmail.com>
2021-06-11 23:29:42 -0400

    skyfield and pip freeze

--- env/serial_gps.py
+++ env/serial_gps.py
@@ -1,4 +1,8 @@
 import serial
+
+from skyfield.api import N, W, load, wgs84
+planets = load('de440s.bsp')  # ephemeris DE421
+
 with serial.Serial('/dev/ttyACM0', baudrate=4800, timeout=1) as ser:
     #x = ser.read()
     #s = ser.read(10)
@@ -11,3 +15,13 @@ print(split_line[3],split_line[4])
 print(split_line[5],split_line[6])
 print(lat)
 print(long)
+
+sun = planets['Sun']
+print(sun)
+
+tracker = earth + wgs84.latlon(lat, long)
+astrometric = tracker.at(t).observe(sun)
+alt, az, d = astrometric.apparent().altaz()
+
+print(alt)
+print(az)
--- readme.md
+++ readme.md
@@ -7,3 +7,10 @@ sudo apt-get install python-smbus
 sudo apt-get install i2c-tools
 
 stty -F /dev/ttyACM0 ispeed 4800 && cat </dev/ttyACM0
+
+
+planets = load('de421.bsp')  # ephemeris DE421
+
+pip freeze > requirements.txt
+
+https://rhodesmill.org/skyfield/toc.html
